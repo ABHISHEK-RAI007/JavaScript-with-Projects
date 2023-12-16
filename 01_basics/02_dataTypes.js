2ee9eae9313eb8b0c87906d527ea8a6945c358d1
ABHISHEK-RAI007 <abhishek.rai0522@gmail.com>
2023-12-16 11:29:04 +0530

    Data type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,46 @@
+"use strict"; // treat all JS code as newer version;
+
+// alert(3 + 3) // we are using  nodejs, not browser;
+
+// console.log(3 + 3); console.log("Hitesh");
+// Readblity 
+console.log(3 + 3); 
+console.log("Hitesh");
+
+// Data Type 
+
+// String
+let name = "Abhishek";
+
+// Number
+let age = 18;
+
+// Boolean
+let isLoggedIn = false;
+
+// standlone value
+let state = null;
+
+// Data Type => Primitive Data Type 
+// number => 2 to power 53
+
+//bigint
+
+// string => ""
+
+// boolean => true/false
+
+// null => standlone value => Empty
+
+// undefined
+
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
